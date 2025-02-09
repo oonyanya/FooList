2cb76f4b43cc5ca3f4961239896c9f7d956957d7
rirufa <test@yahoo.co.jp>
2025-02-09 15:42:00 +0900

    FixedListを実装した

---
+++ List/FixedList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooProject.Collection
+{
+    public class FixedList<T> : IEnumerable<T>
+    {
+        T[] items;
+        int size;
+
+        public FixedList(int capacity = 4)
+        { 
+            items = new T[capacity];
+        }
+
+        public int Count { get { return size; } }
+
+        public void Add(T item)
+        {
+            if (size >= items.Length)
+                throw new InvalidOperationException("capacity over");
+            items[size++] = item;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach(var item in collection)
+                Add(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            InsertRange(index, new T[1] { item });
+        }
+
+        public void InsertRange(int index, ICollection<T> collection)
+        {
+            int collection_length = collection.Count;
+            if (size + collection_length > items.Length)
+            {
+                throw new InvalidOperationException("capacity over");
+            }
+            else
+            {
+                Array.Copy(items, index, items, index + collection.Count, size - index);
+            }
+
+            int i = index;
+            foreach (var item in collection)
+            {
+                items[i++] = item;
+            }
+            size += collection_length;
+        }
+
+        public void RemoveAt(int index)
+        {
+            RemoveRange(index, 1);
+        }
+
+        public void RemoveRange(int index, int count)
+        {
+            if(index < 0)
+                throw new ArgumentOutOfRangeException("index");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+
+            Array.Copy(items, index + count, items, index, size - (index + count));
+            size -= count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < size; i++)
+                yield return items[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < size; i++)
+                yield return items[i];
+        }
+    }
+}
---
+++ UnitTest/FixedListTest.cs
@@ -0,0 +1,110 @@
+﻿using FooProject.Collection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class FixedListTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var list = new FixedList<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            list.Add('4');
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual("1234", string.Concat(list));
+       }
+        [TestMethod]
+        public void AddRageTest()
+        {
+            var list = new FixedList<char>();
+            list.AddRange("123");
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual("123", string.Concat(list));
+        }
+        [TestMethod]
+        public void InsertTest()
+        {
+            var list = new FixedList<char>();
+            list.AddRange("12");
+            list.Insert(0, '3');
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual("312", string.Concat(list));
+
+            list.Insert(1, '4');
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual("3412", string.Concat(list));
+
+            list = new FixedList<char>();
+            list.AddRange("12");
+            list.Insert(2, '3');
+            Assert.AreEqual("123", string.Concat(list));
+        }
+
+        [TestMethod]
+        public void InsertRageTest()
+        {
+            var list = new FixedList<char>();
+            list.AddRange("12");
+            list.InsertRange(1, "34".ToArray());
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual("1342", string.Concat(list));
+
+            list = new FixedList<char>();
+            list.AddRange("12");
+            list.InsertRange(2, "34".ToArray());
+            Assert.AreEqual("1234", string.Concat(list));
+
+            list = new FixedList<char>();
+            list.AddRange("12");
+            list.InsertRange(0, "34".ToArray());
+            Assert.AreEqual("3412", string.Concat(list));
+        }
+
+        [TestMethod]
+        public void RemoveAtTest()
+        {
+            var list = new FixedList<char>();
+            list.AddRange("1234");
+            list.RemoveAt(0);
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual("234", string.Concat(list));
+
+            list.RemoveAt(1);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual("24", string.Concat(list));
+
+            list.RemoveAt(0);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("4", string.Concat(list));
+        }
+
+        [TestMethod]
+        public void RemoveRageTest()
+        {
+            var list = new FixedList<char>();
+            list.AddRange("1234");
+            list.RemoveRange(0, 2);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual("34", string.Concat(list));
+
+            list = new FixedList<char>();
+            list.AddRange("1234");
+            list.RemoveRange(1, 2);
+            Assert.AreEqual("14", string.Concat(list));
+
+            list = new FixedList<char>();
+            list.AddRange("1234");
+            list.RemoveRange(1, 3);
+            Assert.AreEqual("1", string.Concat(list));
+        }
+    }
+}
