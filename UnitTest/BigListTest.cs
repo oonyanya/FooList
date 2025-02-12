bc7159999660a157140a32cccbab415397a47e33
rirufa <test@yahoo.co.jp>
2025-02-12 23:21:22 +0900

    テストの一部を移植した

--- UnitTest/BigListTest.cs
+++ UnitTest/BigListTest.cs
@@ -21,10 +21,7 @@ namespace UnitTest
         public void GetAtTest()
         {
             var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("abcdefgh");
-            buf.AddRange("ijklmnop");
-            buf.AddRange("lqrstuvw");
-            buf.AddRange("xyz");
+            buf.AddRange("abcdefghijklmnoplqrstuvwxyz");
 
             Assert.AreEqual('a', buf[0]);
             Assert.AreEqual('h', buf[7]);
@@ -43,10 +40,7 @@ namespace UnitTest
         public void SetAtTest()
         {
             var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("abcdefgh");
-            buf.AddRange("ijklmnop");
-            buf.AddRange("lqrstuvw");
-            buf.AddRange("xyz");
+            buf.AddRange("abcdefghijklmnoplqrstuvwxyz");
 
             buf[0] = '-';
             Assert.AreEqual('-', buf[0]);
@@ -80,28 +74,65 @@ namespace UnitTest
         [TestMethod]
         public void AddRangeFrontTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRangeToFront("xyz");
-            buf.AddRangeToFront("lqrstuvw");
-            buf.AddRangeToFront("ijklmnop");
-            buf.AddRangeToFront("abcdefgh");
+            const int SIZE = 8000;
+            BigList<int> biglist1 = new BigList<int>();
 
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("abcdefghijklmnoplqrstuvwxyz", output);
+            int i = 1, j = 0;
+            while (i <= SIZE)
+            {
+                int[] array = new int[j];
+                for (int x = 0; x < j; ++x)
+                    array[j - x - 1] = i + x;
+                biglist1.AddRangeToFront(array);
+                i += j;
+                j += 1;
+                if (j == 20)
+                    j = 0;
+            }
+            int size = i - 1;
+
+            Assert.AreEqual(size, biglist1.Count);
+
+            for (i = 1; i <= size; ++i)
+            {
+                Assert.AreEqual(i, biglist1[size - i]);
+            }
+
+            i = size;
+            foreach (int x in biglist1)
+                Assert.AreEqual(i--, x);
         }
 
         [TestMethod]
         public void AddRangeTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("abcdefgh");
-            buf.AddRange("ijklmnop");
-            buf.AddRange("lqrstuvw");
-            buf.AddRange("xyz");
-            buf.AddRange("xyz");
+            const int SIZE = 8000;
+            BigList<int> biglist1 = new BigList<int>();
 
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("abcdefghijklmnoplqrstuvwxyzxyz", output);
+            int i = 1, j = 0;
+            while (i <= SIZE)
+            {
+                int[] array = new int[j];
+                for (int x = 0; x < j; ++x)
+                    array[x] = i + x;
+                biglist1.AddRange(array);
+                i += j;
+                j += 1;
+                if (j == 20)
+                    j = 0;
+            }
+            int size = i - 1;
+
+            Assert.AreEqual(size, biglist1.Count);
+
+            for (i = 1; i <= size; ++i)
+            {
+                Assert.AreEqual(i, biglist1[i - 1]);
+            }
+
+            i = 1;
+            foreach (int x in biglist1)
+                Assert.AreEqual(i++, x);
         }
 
         [TestMethod]
@@ -116,59 +147,45 @@ namespace UnitTest
         [TestMethod]
         public void AddTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.Add('0');
-            buf.Add('1');
-            buf.Add('2');
-            buf.Add('3');
-            buf.Add('4');
-            buf.Add('5');
-            buf.Add('6');
-            buf.Add('7');
-            buf.Add('8');
-            buf.Add('9');
-            buf.Add('a');
-            buf.Add('b');
-            buf.Add('c');
-            buf.Add('d');
-            buf.Add('e');
-            buf.Add('f');
-            buf.Add('g');
-            buf.Add('h');
-            buf.Add('i');
-            buf.Add('j');
-            buf.Add('k');
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("0123456789abcdefghijk", output);
+            const int SIZE = 8000;
+            BigList<int> biglist1 = new BigList<int>();
+            int i;
+
+            for (i = 1; i <= SIZE; ++i)
+            {
+                biglist1.Add(i);
+            }
+
+            for (i = 1; i <= SIZE; ++i)
+            {
+                Assert.AreEqual(i, biglist1[i - 1]);
+            }
+
+            i = 1;
+            foreach (int x in biglist1)
+                Assert.AreEqual(i++, x);
         }
 
         [TestMethod]
         public void AddFrontTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.Add('k');
-            buf.AddToFront('j');
-            buf.AddToFront('i');
-            buf.AddToFront('h');
-            buf.AddToFront('g');
-            buf.AddToFront('f');
-            buf.AddToFront('e');
-            buf.AddToFront('d');
-            buf.AddToFront('c');
-            buf.AddToFront('b');
-            buf.AddToFront('a');
-            buf.AddToFront('9');
-            buf.AddToFront('8');
-            buf.AddToFront('7');
-            buf.AddToFront('6');
-            buf.AddToFront('5');
-            buf.AddToFront('4');
-            buf.AddToFront('3');
-            buf.AddToFront('2');
-            buf.AddToFront('1');
-            buf.AddToFront('0');
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("0123456789abcdefghijk", output);
+            const int SIZE = 8000;
+            BigList<int> biglist1 = new BigList<int>();
+            int i;
+
+            for (i = 1; i <= SIZE; ++i)
+            {
+                biglist1.AddToFront(i);
+            }
+
+            for (i = 1; i <= SIZE; ++i)
+            {
+                Assert.AreEqual(i, biglist1[SIZE - i]);
+            }
+
+            i = SIZE;
+            foreach (int x in biglist1)
+                Assert.AreEqual(i--, x);
         }
 
         [TestMethod]
@@ -206,30 +223,74 @@ namespace UnitTest
         [TestMethod]
         public void InsertTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("0123456789");
-            buf.Insert(8, ';');
-            buf.Insert(4, 'a');
-            buf.Insert(5, 'b');
-            buf.Insert(6, 'c');
-            buf.Insert(7, 'd');
-            buf.Insert(8, 'e');
-            buf.Insert(9, 'f');
-            buf.Insert(10, 'g');
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("0123abcdefg4567;89", output);
+            BigList<int> list1, list2, list3;
+
+            list1 = new BigList<int>();
+            list1.Insert(0, 34);
+            Assert.AreEqual(1, list1.Count);
+            Assert.AreEqual(34, list1[0]);
+            list1.Insert(1, 78);
+            list1.Insert(0, 11);
+            list1.Insert(1, 13);
+            InterfaceTests.TestEnumerableElements<int>(list1, new int[] { 11, 13, 34, 78 });
+
+            list2 = CreateList(0, 100);
+            int j = 300;
+            for (int i = 0; i < list2.Count; i += 3)
+                list2.Insert(i, j++);
+
+            int k = 0;
+            j = 300;
+            for (int i = 0; i < list2.Count; ++i)
+            {
+                if (i % 3 == 0)
+                {
+                    Assert.AreEqual(j++, list2[i]);
+                }
+                else
+                {
+                    Assert.AreEqual(k++, list2[i]);
+                }
+            }
+
+            list3 = new BigList<int>();
+            for (int i = 0; i < 32; ++i)
+                list3.Add(i);
+
+            list3.Insert(24, 101);
+            list3.Insert(16, 100);
+            list3.Insert(8, 102);
+            InterfaceTests.TestEnumerableElements<int>(list3, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 102, 8, 9, 10, 11, 12, 13, 14, 15, 100, 16, 17, 18, 19, 20, 21, 22, 23, 101, 24, 25, 26, 27, 28, 29, 30, 31 });
         }
 
         [TestMethod]
         public void InserRangetTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("0123456789");
-            buf.InsertRange(8, ";:");
-            buf.InsertRange(4, "abcdef");
-            buf.InsertRange(10, "gjiklmn");
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("0123abcdefgjiklmn4567;:89", output);
+            BigList<int> list2;
+            IEnumerable<int> e1;
+            list2 = CreateList(0, 20);
+            e1 = new int[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1 };
+            list2.InsertRange(0, e1);
+            list2.InsertRange(17, e1);
+            InterfaceTests.TestEnumerableElements<int>(list2, new int[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
+
+            list2 = CreateList(0, 20);
+            e1 = new int[] { -10, -9 };
+            list2.InsertRange(0, e1);
+            list2.InsertRange(17, e1);
+            InterfaceTests.TestEnumerableElements<int>(list2, new int[] { -10, -9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, -10, -9, 15, 16, 17, 18, 19 });
+
+            list2 = new BigList<int>();
+            list2.AddRange(new int[] { 1, 2, 3, 4, 5 });
+            list2.InsertRange(2, new int[] { 9, 8 });
+            InterfaceTests.TestEnumerableElements<int>(list2, new int[] { 1, 2, 9, 8, 3, 4, 5 });
+
+            list2 = new BigList<int>();
+            list2.Add(1);
+            list2.Add(2);
+            list2.InsertRange(1, new int[] { 6, 5, 4 });
+            list2.InsertRange(2, new int[] { 9, 8 });
+            InterfaceTests.TestEnumerableElements<int>(list2, new int[] { 1, 6, 9, 8, 5, 4, 2 });
         }
 
         [TestMethod]
@@ -249,34 +310,86 @@ namespace UnitTest
         [TestMethod]
         public void RemoveRangeTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("0123456789");
-            buf.InsertRange(5, "abcdef");
-            buf.RemoveRange(5, 7);
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("012346789", output);
-            buf.RemoveRange(0, 7);
-            output = String.Concat<char>(buf);
-            Assert.AreEqual("89", output);
+            BigList<int> list1 = new BigList<int>();
+            for (int i = 0; i < 200; ++i)
+                list1.Add(i);
+
+            list1.RemoveRange(0, 5);
+            list1.RemoveRange(194, 1);
+            list1.RemoveRange(50, 0);
+            list1.RemoveRange(30, 25);
+            list1.RemoveRange(120, 37);
+
+            InterfaceTests.TestEnumerableElements<int>(list1, new int[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                32, 33, 34, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+                96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124,
+                125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 187, 188, 189,
+                190, 191, 192, 193, 194, 195, 196, 197, 198 });
+
+            list1 = CreateList(0, 200);
+
+            list1.RemoveRange(0, 5);
+            list1.RemoveRange(194, 1);
+            list1.RemoveRange(50, 0);
+            list1.RemoveRange(30, 25);
+            list1.RemoveRange(120, 37);
+
+            InterfaceTests.TestEnumerableElements<int>(list1, new int[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                32, 33, 34, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+                96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124,
+                125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 187, 188, 189,
+                190, 191, 192, 193, 194, 195, 196, 197, 198 });
+        }
+
+        BigList<int> CreateList(int start, int length)
+        {
+            if (length < 24)
+            {
+                int[] array = new int[length];
+                for (int i = 0; i < length; ++i)
+                    array[i] = i + start;
+                var collection = new BigList<int>();
+                collection.AddRange(array);
+                return collection;
+            }
+            else
+            {
+                int split = length / 5 * 2;
+                var collection = CreateList(start, split);
+                var other_list = CreateList(start + split, length - split);
+                collection.AddRange(other_list);
+                return collection;
+            }
         }
 
         [TestMethod]
         public void RemoveAtTest()
         {
-            var buf = new FooProject.Collection.BigList<char>();
-            buf.AddRange("01234567"+"89");
-            buf.RemoveAt(7);
-            var output = String.Concat<char>(buf);
-            Assert.AreEqual("012345689", output);
-            buf.RemoveAt(7);
-            output = String.Concat<char>(buf);
-            Assert.AreEqual("01234569", output);
-            buf.RemoveAt(0);
-            output = String.Concat<char>(buf);
-            Assert.AreEqual("1234569", output);
-            buf.RemoveAt(6);
-            output = String.Concat<char>(buf);
-            Assert.AreEqual("123456", output);
+            BigList<int> list1 = new BigList<int>();
+            for (int i = 0; i < 100; ++i)
+                list1.Add(i);
+
+            for (int i = 0; i < 50; ++i)
+                list1.RemoveAt(50);
+
+            list1.RemoveAt(0);
+
+            for (int i = 1; i < list1.Count; i += 2)
+                list1.RemoveAt(i);
+
+            InterfaceTests.TestEnumerableElements<int>(list1, new int[] { 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36, 37, 39, 40, 42, 43, 45, 46, 48, 49 });
+
+            list1 = CreateList(0, 100);
+
+            for (int i = 0; i < 50; ++i)
+                list1.RemoveAt(50);
+
+            list1.RemoveAt(0);
+
+            for (int i = 1; i < list1.Count; i += 2)
+                list1.RemoveAt(i);
+
+            InterfaceTests.TestEnumerableElements<int>(list1, new int[] { 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36, 37, 39, 40, 42, 43, 45, 46, 48, 49 });
         }
     }
 }
---
+++ UnitTest/InterfaceTests.cs
@@ -0,0 +1,57 @@
+﻿//******************************
+// Written by Peter Golde
+// Copyright (c) 2004-2007, Wintellect
+//
+// Use and restribution of this code is subject to the license agreement 
+// contained in the file "License.txt" accompanying this file.
+//******************************
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// The BinaryPredicate delegate type  encapsulates a method that takes two
+    /// items of the same type, and returns a boolean value representating 
+    /// some relationship between them. For example, checking whether two
+    /// items are equal or equivalent is one kind of binary predicate.
+    /// </summary>
+    /// <param name="item1">The first item.</param>
+    /// <param name="item2">The second item.</param>
+    /// <returns>Whether item1 and item2 satisfy the relationship that the BinaryPredicate defines.</returns>
+    public delegate bool BinaryPredicate<T>(T item1, T item2);
+    
+    /// <summary>
+    /// A collection of generic tests for various interfaces.
+    /// </summary>
+    internal static class InterfaceTests
+    {
+        /// <summary>
+        /// Test an IEnumerable should contain the given values in order
+        /// </summary>
+        public static void TestEnumerableElements<T>(IEnumerable<T> e, T[] expected)
+        {
+            TestEnumerableElements<T>(e, expected, null);
+        }
+
+        public static void TestEnumerableElements<T>(IEnumerable<T> e, T[] expected, BinaryPredicate<T> equals)
+        {
+            if (equals == null)
+                equals = delegate (T x, T y) { return object.Equals(x, y); };
+
+            int i = 0;
+            foreach (T item in e)
+            {
+                Assert.IsTrue(equals(expected[i], item));
+                ++i;
+            }
+            Assert.AreEqual(expected.Length, i);
+        }
+    }
+}
