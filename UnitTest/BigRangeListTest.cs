7667168f5b9c5aa2860bd722774e5fc43c06c40d
rirufa <test@yahoo.co.jp>
2025-03-15 22:00:28 +0900

    テストをいくつか追加した

--- UnitTest/BigRangeListTest.cs
+++ UnitTest/BigRangeListTest.cs
@@ -53,6 +53,112 @@ namespace UnitTest
             AssertAreRangeEqual(expected, list);
         }
 
+        [TestMethod]
+        public void InsertTest()
+        {
+            BigRangeList<MyRange> list = new BigRangeList<MyRange>();
+            list.Add(new MyRange(0, 3));
+            list.Add(new MyRange(0, 3));
+            for (int i = 0; i < 8; i++)
+            {
+                list.Insert(1,new MyRange(0, 3));
+            }
+            var expected = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, };
+            AssertAreRangeEqual(expected, list);
+
+            list = new BigRangeList<MyRange>();
+            list.Add(new MyRange(0, 3));
+            list.Add(new MyRange(0, 3));
+            for (int i = 0; i < 8; i++)
+            {
+                list.Insert(0, new MyRange(0, 3));
+            }
+            expected = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, };
+            AssertAreRangeEqual(expected, list);
+
+            list = new BigRangeList<MyRange>();
+            list.Add(new MyRange(0, 3));
+            list.Add(new MyRange(0, 3));
+            for (int i = 0; i < 8; i++)
+            {
+                list.Insert(list.Count, new MyRange(0, 3));
+            }
+            expected = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, };
+            AssertAreRangeEqual(expected, list);
+        }
+
+        [TestMethod]
+        public void InsertRangeTest()
+        {
+            var rangeList = new List<MyRange>();
+            for (int i = 0; i < 8; i++)
+            {
+                rangeList.Add(new MyRange(0, 3));
+            }
+
+            var list = new BigRangeList<MyRange>();
+            list.Add(new MyRange(0, 3));
+            list.Add(new MyRange(0, 3));
+            list.InsertRange(1,rangeList);
+
+            var expected = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, };
+            AssertAreRangeEqual(expected, list);
+
+            list = new BigRangeList<MyRange>();
+            list.Add(new MyRange(0, 3));
+            list.Add(new MyRange(0, 3));
+            list.InsertRange(0, rangeList);
+
+            expected = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, };
+            AssertAreRangeEqual(expected, list);
+
+            list = new BigRangeList<MyRange>();
+            list.Add(new MyRange(0, 3));
+            list.Add(new MyRange(0, 3));
+            list.InsertRange(list.Count, rangeList);
+
+            expected = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, };
+            AssertAreRangeEqual(expected, list);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            var rangeList = new List<MyRange>();
+            for (int i = 0; i < 10; i++)
+            {
+                rangeList.Add(new MyRange(0, 3));
+            }
+
+            var list = new BigRangeList<MyRange>();
+            list.AddRange(rangeList);
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            list.RemoveAt(2);
+            list.RemoveAt(3);
+            list.RemoveAt(5);
+
+            var expected = new int[] { 0, 3, 6, 9, 12, 15,   };
+            AssertAreRangeEqual(expected, list);
+        }
+
+        [TestMethod]
+        public void RemoveRangeTest()
+        {
+            var rangeList = new List<MyRange>();
+            for (int i = 0; i < 10; i++)
+            {
+                rangeList.Add(new MyRange(0, 3));
+            }
+
+            var list = new BigRangeList<MyRange>();
+            list.AddRange(rangeList);
+            list.RemoveRange(0, 2);
+            list.RemoveRange(2, 2);
+            list.RemoveRange(5, 1);
+            var expected = new int[] { 0, 3, 6, 9, 12, 15, };
+            AssertAreRangeEqual(expected, list);
+        }
     }
 
     class MyRange : IRange
