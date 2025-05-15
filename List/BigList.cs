8578d24b23e435e2f6935d90db7e2c084ad18006
rirufa <test@yahoo.co.jp>
2025-05-16 08:40:26 +0900

    名前空間を移動した

--- List/BigList.cs
+++ List/BigList.cs
@@ -16,6 +16,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.VisualBasic;
+using FooProject.Collection.DataStore;
 
 namespace FooProject.Collection
 {
--- List/PinableContainer.cs
+++ List/PinableContainer.cs
@@ -9,18 +9,27 @@ namespace FooProject.Collection
     public class PinableContainer<T>
     {
         public long Index { get; private set; }
+        public long Length { get; private set; }
         public T Content { get; private set; }
 
         public PinableContainer(T content)
         {
             this.Content = content;
             this.Index = -1;
+            this.Length = -1;
         }
 
         public void SetConent(long index, T content)
         {
             this.Content = content;
             this.Index = index;
+        }
+
+        public void SetConent(long index, T content,long length)
+        {
+            this.Content = content;
+            this.Index = index;
+            this.Length = length;
         }
 
         public void RemoveContent()
