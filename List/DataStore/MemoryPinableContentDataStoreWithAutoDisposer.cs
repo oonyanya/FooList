79c4196cee41f5ebfa090400606922b799e41602
rirufa <test@yahoo.co.jp>
2025-06-13 17:20:36 +0900

    MemoryPinableContentDataStoreWithAutoDisposerクラスを追加した

---
+++ List/DataStore/MemoryPinableContentDataStoreWithAutoDisposer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooProject.Collection.DataStore
+{
+    public class MemoryPinableContentDataStoreWithAutoDisposer<T> : IPinableContainerStore<T>
+    {
+        EmptyList emptyList = new EmptyList();
+        bool disposedValue = false;
+        CacheList<long, PinableContainer<T>> writebackCacheList = null;
+
+        public MemoryPinableContentDataStoreWithAutoDisposer(int cache_limit = 128)
+        {
+            this.writebackCacheList = new CacheList<long, PinableContainer<T>>();
+            this.writebackCacheList.Limit = cache_limit;
+            this.writebackCacheList.CacheOuted += (outed_index, outed_item) => {
+                this.OnDispoing(outed_item.Content);
+            };
+        }
+
+        public event Action<T> Dispoing;
+
+        public void OnDispoing(T item)
+        {
+            if (this.Dispoing != null)
+                this.Dispoing(item);
+        }
+
+        public IEnumerable<T> ForEachAvailableContent()
+        {
+            foreach (var pinableContainer in this.writebackCacheList.ForEachValue())
+            {
+                if (pinableContainer.CacheIndex != PinableContainer<T>.NOTCACHED || pinableContainer.Content?.Equals(default(T)) == false)
+                {
+                    yield return pinableContainer.Content;
+                }
+            }
+        }
+
+        public PinnedContent<T> Get(PinableContainer<T> pinableContainer)
+        {
+            PinnedContent<T> result;
+            if (TryGet(pinableContainer, out result))
+                return result;
+            else
+                throw new ArgumentException();            
+        }
+
+        public bool TryGet(PinableContainer<T> pinableContainer, out PinnedContent<T> result)
+        {
+            result = new PinnedContent<T>(pinableContainer,this);
+            return true;
+        }
+
+        public void Set(PinableContainer<T> pinableContainer)
+        {
+            if (pinableContainer.IsRemoved)
+            {
+                this.emptyList.SetID(pinableContainer.CacheIndex);
+                pinableContainer.CacheIndex = PinableContainer<T>.NOTCACHED;
+                return;
+            }
+
+            if (pinableContainer.CacheIndex == PinableContainer<T>.NOTCACHED)
+            {
+                pinableContainer.CacheIndex = this.emptyList.GetID();
+            }
+
+            this.writebackCacheList.Set(pinableContainer.CacheIndex, pinableContainer);
+            return;
+        }
+
+        public void Commit()
+        {
+            this.writebackCacheList.Flush();
+        }
+
+        public void Dispose()
+        {
+            //GC前にプログラム的にリソースを破棄するので
+            //管理,非管理リソース両方が破棄されるようにする
+            Dispose(true);
+            GC.SuppressFinalize(this);//破棄処理は完了しているのでGC不要の合図
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposedValue)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                //管理リソースの破棄処理
+                this.writebackCacheList.Flush();
+            }
+
+            //非管理リソースの破棄処理
+
+            disposedValue = true;
+        }
+
+        ~MemoryPinableContentDataStoreWithAutoDisposer()
+        {
+            //GC時に実行されるデストラクタでは非管理リソースの削除のみ
+            Dispose(false);
+        }
+    }
+}
---
+++ UnitTest/MemoryPinableContentDataStoreWithAutoDisposerTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FooProject.Collection.DataStore;
+
+namespace UnitTest
+{
+    public class TestContent : IDisposable
+    {
+        public TestContent(int n)
+        {
+            Disposed = false;
+            Value = n;
+        }
+
+        public int Value { get; set; }
+
+        public bool Disposed { get; set; }
+
+        public void Dispose()
+        {
+            Disposed = true;
+        }
+    }
+
+    [TestClass]
+    public class MemoryPinableContentDataStoreWithAutoDisposerTest
+    {
+        [TestMethod]
+        public void SetTest()
+        {
+            var test_data = new int[] { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000 };
+
+            List<PinableContainer<TestContent>> containers = new List<PinableContainer<TestContent>>();
+            int disposedCount = 0;
+            var disk = new MemoryPinableContentDataStoreWithAutoDisposer<TestContent>(2);
+            disk.Dispoing += (o) =>
+            {
+                disposedCount++;
+            };
+            foreach (var item in test_data)
+            {
+                var data = new PinableContainer<TestContent>(new TestContent(item));
+                disk.Set(data);
+
+                containers.Add(data);
+            }
+
+            Assert.IsTrue(disposedCount > 0);
+
+            disk.Dispose();
+        }
+
+        [TestMethod]
+        public void ForEachAvailableContentTest()
+        {
+            var disk = new MemoryPinableContentDataStoreWithAutoDisposer<TestContent>(2);
+            var test_data = new int[] { 100, 200, 300, 400 };
+            List<PinableContainer<TestContent>> containers = new List<PinableContainer<TestContent>>();
+
+            foreach (var item in test_data)
+            {
+                var data = new PinableContainer<TestContent>(new TestContent(item));
+                disk.Set(data);
+                containers.Add(data);
+            }
+
+            var expected_data = new int[] { 400, 300 };
+            int i = 0;
+            foreach (var item in disk.ForEachAvailableContent())
+            {
+                Assert.AreEqual(expected_data[i], item.Value);
+                i++;
+            }
+
+            disk.Commit();
+
+            Assert.AreEqual(0, disk.ForEachAvailableContent().Count());
+
+            disk.Dispose();
+        }
+
+        [TestMethod]
+        public void CommitTest()
+        {
+            int disposedCount = 0;
+            var disk = new MemoryPinableContentDataStoreWithAutoDisposer<TestContent>(10);
+            disk.Dispoing += (o) =>
+            {
+                disposedCount++;
+            };
+            var test_data = new int[] { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000 };
+            List<PinableContainer<TestContent>> containers = new List<PinableContainer<TestContent>>();
+            foreach (var item in test_data)
+            {
+                var data = new PinableContainer<TestContent>(new TestContent (item));
+                disk.Set(data);
+                containers.Add(data);
+            }
+
+            disk.Commit();
+
+            Assert.IsTrue(disposedCount > 0);
+
+            disk.Dispose();
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            var serializer = new TestSerializer();
+            var disk = new MemoryPinableContentDataStoreWithAutoDisposer<TestContent>(2);
+            var test_data = new int[] { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000 };
+            List<PinableContainer<TestContent>> containers = new List<PinableContainer<TestContent>>();
+            foreach (var item in test_data)
+            {
+                var data = new PinableContainer<TestContent>(new TestContent (item));
+                disk.Set(data);
+                containers.Add(data);
+            }
+
+            int i = 0;
+            foreach (var data in containers)
+            {
+                var pinned = disk.Get(data);
+                Assert.AreEqual(test_data[i], pinned.Content.Value);
+                pinned.Dispose();
+                i++;
+            }
+
+            disk.Dispose();
+        }
+    }
+}
