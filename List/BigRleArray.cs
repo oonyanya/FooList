639461c46f6275526aeac2f6e4e4c1e809427e96
rirufa <test@yahoo.co.jp>
2026-04-18 09:26:27 +0900

    RleArrayを実装した

---
+++ List/BigRleArray.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooProject.Collection
+{
+    public interface IRleArrayRange<J> : IRange
+    {
+        J Value { get; set; }
+    }
+    public class BigRleArray<T, J>: IEnumerable<T> where  T : IRleArrayRange<J>, new()
+    {
+        BigRangeList<T> _rleData = new BigRangeList<T>();
+
+        /// <summary>
+        /// アイテムを追加する
+        /// </summary>
+        /// <param name="item">追加するアイテム</param>
+        /// <remarks>既に存在するアイテムの場合、アイテムの長さが変わります</remarks>
+        public void AddOrUpdate(J item)
+        {
+            if (_rleData.Count > 0)
+            {
+                var last = _rleData[_rleData.Count - 1];
+                if (last.Value.Equals(item))
+                {
+                    last.length++;
+                    _rleData[_rleData.Count - 1] = last;
+                }
+                else
+                {
+                    var new_value = new T();
+                    new_value.Value = item;
+                    new_value.length = 1;
+                    _rleData.Add(new_value);
+                }
+            }
+            else
+            {
+                var new_value = new T();
+                new_value.Value = item;
+                new_value.length = 1;
+                _rleData.Add(new_value);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(var item in _rleData)
+                yield return item;
+        }
+
+        /// <summary>
+        /// アイテムを挿入します
+        /// </summary>
+        /// <param name="absolute_index">挿入対象の絶対インデックス</param>
+        /// <param name="item">挿入対象のアイテム</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <remarks>既に存在するアイテムの場合、アイテムの長さが変わります。また、既に存在するアイテムに別のアイテムを挿入した場合、挿入した箇所が上書きされます</remarks>
+        public void InsertOrUpdate(int absolute_index, J item)
+        {
+            if (absolute_index == _rleData.Count)
+            {
+                AddOrUpdate(item);
+                return;
+            }
+
+            var i = _rleData.GetIndexFromAbsoluteIndexIntoRange(absolute_index);
+            if(i == -1)
+                throw new InvalidOperationException("absoulte range is invaild");
+
+            var container = _rleData.Get(i);
+            if (container.Value.Equals(item))
+            {
+                container.length++;
+                _rleData.Set(i, container);
+            }
+            else
+            {
+                var CustomConverter = (RangeConverter<T>)_rleData.LeastFetchStore;
+                var converter = CustomConverter.customLeastFetch.absoluteIndexIntoRange;
+                var offset = absolute_index - container.start;
+
+                if(offset > 0)
+                {
+                    _rleData.Set(i, new T() { Value = container.Value, length = offset });
+                    _rleData.Insert(i + 1, new T() { Value = item, length = 1 });
+                    _rleData.Insert(i + 2, new T() { Value = container.Value, length = container.length - offset - 1 });  // -1は挿入した分
+                }
+                else
+                {
+                    _rleData.Set(i, new T() { Value = item, length = 1 });
+                    _rleData.Insert(i + 1, new T() { Value = container.Value, length = container.length - 1 });  // -1は挿入した分
+                }
+            }
+        }
+
+        /// <summary>
+        /// アイテムを削除します
+        /// </summary>
+        /// <param name="absolute_index">削除するインデックス</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <remarks>アイテムの長さを変えます。0になった場合、アイテム自体が削除されます。</remarks>
+        public void Remove(int absolute_index)
+        {
+            var i = _rleData.GetIndexFromAbsoluteIndexIntoRange(absolute_index);
+            if (i == -1)
+                throw new InvalidOperationException("absoulte range is invaild");
+
+            var container = _rleData.Get(i);
+            if (container.length == 1)
+            {
+                _rleData.RemoveAt(i);
+            }
+            else
+            {
+                container.length--;
+                _rleData.Set(i, container);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
---
+++ UnitTest/BigRleArrayTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FooProject.Collection;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class BigRleArrayTest
+    {
+        class CharRleArray : IRleArrayRange<char>
+        {
+            public char Value { get; set; }
+            public long start { get; set; }
+            public long length { get; set; }
+
+            public CharRleArray()
+            {
+            }
+
+            public CharRleArray(char v,long length)
+            {
+                this.Value = v;
+                this.length = length;
+            }
+
+            public IRange DeepCopy()
+            {
+                var new_item = new CharRleArray();
+                new_item.start = start;
+                new_item.Value  = Value;
+                new_item.length = length;
+                return new_item;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                var other = obj as CharRleArray;
+                if (other == null) return false;
+                if(other.Value == this.Value && other.length == this.length) return true;
+                return false;
+            }
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var list = new BigRleArray<CharRleArray, char>();
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('b');
+            list.AddOrUpdate('b');
+
+            var expected_list = new CharRleArray[] { new CharRleArray('a', 3), new CharRleArray('b', 2) };
+            InterfaceTests.TestEnumerableElements<CharRleArray>(list, expected_list);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var list = new BigRleArray<CharRleArray, char>();
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('b');
+            list.AddOrUpdate('b');
+            list.AddOrUpdate('b');
+            list.AddOrUpdate('b');
+            list.InsertOrUpdate(1, 'a');
+            list.InsertOrUpdate(4, 'c');
+
+            var expected_list = new CharRleArray[] { new CharRleArray('a', 4),  new CharRleArray('c', 1), new CharRleArray('b', 3) };
+            InterfaceTests.TestEnumerableElements<CharRleArray>(list, expected_list);
+
+            list.InsertOrUpdate(6, 'd');
+            expected_list = new CharRleArray[] { new CharRleArray('a', 4), new CharRleArray('c', 1), new CharRleArray('b', 1), new CharRleArray('d', 1), new CharRleArray('b', 1) };
+            InterfaceTests.TestEnumerableElements<CharRleArray>(list, expected_list);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            var list = new BigRleArray<CharRleArray, char>();
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('a');
+            list.AddOrUpdate('b');
+            list.AddOrUpdate('b');
+
+            list.Remove(1);
+
+            var expected_list = new CharRleArray[] { new CharRleArray('a', 2), new CharRleArray('b', 2) };
+            InterfaceTests.TestEnumerableElements<CharRleArray>(list, expected_list);
+
+            list.Remove(0);
+            list.Remove(0);
+            expected_list = new CharRleArray[] { new CharRleArray('b', 2) };
+            InterfaceTests.TestEnumerableElements<CharRleArray>(list, expected_list);
+        }
+    }
+}
