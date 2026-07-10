b648babe24a225da81a36ec68c9f7fef65fb7a87
rirufa <test@yahoo.co.jp>
2026-07-10 17:06:11 +0900

    テストパターンを追加した

--- UnitTest/EnumWithFlagsGeneratorTest.cs
+++ UnitTest/EnumWithFlagsGeneratorTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FooProject.Collection;
 using FooProject.Generator;
+using static UnitTest.BigRleArrayTest;
 
 namespace UnitTest
 {
@@ -36,10 +38,27 @@ namespace UnitTest
             Assert.AreEqual(TestMarker.Important, collection.Get(1001));
             Assert.AreEqual(TestMarker.Mark, collection.Get(10));
 
+            var expected_list = new BigRleArrayRange<TestMarker>[] { 
+                new BigRleArrayRange<TestMarker>(TestMarker.None, 0, 10), 
+                new BigRleArrayRange<TestMarker>(TestMarker.Mark, 10, 500),
+                new BigRleArrayRange<TestMarker>(TestMarker.None, 510, 490),
+                new BigRleArrayRange<TestMarker>(TestMarker.Important, 1000, 100)
+            };
+            InterfaceTests.TestEnumerableElements<IRleArrayRange<TestMarker>>(collection.GetRanges(0,1100), expected_list);
+
+
             collection.Unset(10, 500, TestMarker.Mark);
             Assert.AreEqual(TestMarker.None, collection.Get(0));
             Assert.AreEqual(TestMarker.None, collection.Get(10));
             Assert.AreEqual(TestMarker.Important, collection.Get(1001));
+
+            expected_list = new BigRleArrayRange<TestMarker>[] {
+                new BigRleArrayRange<TestMarker>(TestMarker.None, 0, 10),
+                new BigRleArrayRange<TestMarker>(TestMarker.None, 10, 500),
+                new BigRleArrayRange<TestMarker>(TestMarker.None, 510, 490),
+                new BigRleArrayRange<TestMarker>(TestMarker.Important, 1000, 100)
+            };
+            InterfaceTests.TestEnumerableElements<IRleArrayRange<TestMarker>>(collection.GetRanges(0, 1100), expected_list);
         }
     }
 }
