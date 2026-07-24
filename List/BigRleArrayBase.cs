507ed5b41d8b3393643ab8c9d3ffe7b98701993c
rirufa <test@yahoo.co.jp>
2026-07-24 14:21:49 +0900

    BigRleArrayが空の時にInsertを実行すると落ちるバグを修正した

--- List/BigRleArrayBase.cs
+++ List/BigRleArrayBase.cs
@@ -250,7 +250,7 @@ namespace FooProject.Collection
         public void Insert(IRleArrayRange<T> item)
         {
             var absolute_index = item.start;
-            if (absolute_index == _rleData.TotalRangeCount)
+            if (_rleData.Count == 0 || absolute_index == _rleData.TotalRangeCount)
             {
                 Add(item);
                 return;
